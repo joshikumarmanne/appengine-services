709bda47f1bc6b41a94471f2e73607725625c390
joshi <joshi@heresy.in>
2017-01-11 17:44:20 +0530

    initial module setup.

---
+++ base.py
@@ -0,0 +1,5 @@
+import endpoints
+import os
+reachmore_api = endpoints.api(name='reachmore', version='v1')
+
+REACHMORE = reachmore_api
---
+++ main.py
@@ -0,0 +1,3 @@
+import endpoints
+from base import REACHMORE
+APPLICATION = endpoints.api_server([REACHMORE])
